6e9bf57dc3ed09b0cd504089e6f7322084d6568c
James Garcia Otero <jamgarc@microsoft.com>
2020-04-19 11:50:50 -0700

    Added additional solutions to ReverseInteger.

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -12,12 +12,38 @@ int reverse(int x) {
         if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && i > 7)) {
             return 0;
         }
-        else if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && i < -8)) {
+        if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && i < -8)) {
             return 0;
         }
-        else {
-            ans = ans * 10 + i;
-        }
+        ans = ans * 10 + i;
     }
     return ans;
+}
+
+//use long to hold value
+int reverse(int x) {
+    long rev = 0;
+    while (x != 0) {
+        int back = x % 10;
+        x /= 10;
+        rev = rev * 10 + back;
+    }
+    if (rev < INT_MIN || rev > INT_MAX)
+        return 0;
+    return rev;
+}
+
+//This is what I would give
+int reverse(int x) {
+    int reverse(int x) {
+        string s = to_string(x);
+        if (s[0] == '-')
+            ::reverse(s.begin() + 1, s.end());
+        else
+            ::reverse(s.begin(), s.end());
+        long rev = stol(s);
+        if (rev < INT_MIN || rev > INT_MAX)
+            return 0;
+        return rev;
+    }
 }
