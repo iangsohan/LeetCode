41fe56974ca70d3fa9465ee208f7049c24235719
iangsohan <iangsohan@gmail.com>
2020-04-19 16:37:53 -0400

    Update PalindromeNumber.cpp
    Simplifies original post, also offers more readable option.

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -5,31 +5,17 @@
 #include<string>
 using namespace std;
 
+// uses less memory, runs faster, less readable
 bool isPalindrome(int x) {
     string s = to_string(x);
-    int j = s.length() - 1;
-    for (int i = 0; i < s.length() / 2; i++) {
-        if (s[i] == s[j]) {
-            j--;
-        } else {
-            return false;
-        }
-    }
-    return true;
-}
-
-//Cached s.length() so it's not called repeatedly
-//Took advantage of c++ for loop syntax to not let j be valid elsewhere/simplify
-bool isPalindrome(int x) {
-    string s = to_string(x);
-    for (int i = 0, j = s.length()-1; i < s.length() / 2; i++, j--) {
+    for (int i = 0, j = s.length() - 1; i < s.length() / 2; i++, j--) {
         if (s[i] != s[j])
             return false;
     }
     return true;
 }
 
-//Used Standard library to make code readable (uses more memory and slower)
+// uses more memory, runs slower, more readable
 bool isPalindrome(int x) {
     string num = to_string(x);
     string reversedNum{ num };
