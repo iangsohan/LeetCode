7a6add5c16e1b1062aec33d6532d5555e41064b2
iangsohan <iangsohan@gmail.com>
2020-04-19 17:48:04 -0400

    Update RomanToInteger.cpp
    Cleans up the mess of too many if statements.

--- RomanToInteger.cpp
+++ RomanToInteger.cpp
@@ -2,58 +2,23 @@
 	- Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.	*/
 
 #include<iostream>
+#include<unordered_map>
 using namespace std;
 
 int romanToInt(string s) {
-    int ans = 0;
-    int tmp = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'I') {
-            ans++;
-            tmp = 1;
-        } else if (s[i] == 'V') {
-            ans += 5;
-            if (tmp < 5) {
+    int romanToInt(string s) {
+        int ans = 0;
+        int tmp = 0;
+        unordered_map<char, int> umap = { {'I', 1},{'V', 5}, {'X', 10}, {'L', 50},
+                                         {'C', 100}, {'D', 500}, {'M', 1000} };
+        for (int i = 0; i < s.length(); i++) {
+            ans += umap[s[i]];
+            if (tmp < umap[s[i]]) {
                 ans -= tmp * 2;
                 tmp = 0;
             }
-            tmp = 5;
-        } else if (s[i] == 'X') {
-            ans += 10;
-            if (tmp < 10) {
-                ans -= tmp * 2;
-                tmp = 0;
-            }
-            tmp = 10;
-        } else if (s[i] == 'L') {
-            ans += 50;
-            if (tmp < 50) {
-                ans -= tmp * 2;
-                tmp = 0;
-            }
-            tmp = 50;
-        } else if (s[i] == 'C') {
-            ans += 100;
-            if (tmp < 100) {
-                ans -= tmp * 2;
-                tmp = 0;
-            }
-            tmp = 100;
-        } else if (s[i] == 'D') {
-            ans += 500;
-            if (tmp < 500) {
-                ans -= tmp * 2;
-                tmp = 0;
-            }
-            tmp = 500;
-        } else if (s[i] == 'M') {
-            ans += 1000;
-            if (tmp < 1000) {
-                ans -= tmp * 2;
-                tmp = 0;
-            }
-            tmp = 1000;
+            tmp = umap[s[i]];
         }
+        return ans;
     }
-    return ans;
 }
