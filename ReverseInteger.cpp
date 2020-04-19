6c1e06fb7669d2069eaf7f7ec573dc098e009df5
iangsohan <iangsohan@gmail.com>
2020-04-19 16:41:51 -0400

    Update ReverseInteger.cpp
    Simplifies from double if statements, also offers additional option.

--- ReverseInteger.cpp
+++ ReverseInteger.cpp
@@ -5,23 +5,6 @@
 using namespace std;
 
 int reverse(int x) {
-    int ans = 0;
-    while (x != 0) {
-        int i = x % 10;
-        x /= 10;
-        if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && i > 7)) {
-            return 0;
-        }
-        if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && i < -8)) {
-            return 0;
-        }
-        ans = ans * 10 + i;
-    }
-    return ans;
-}
-
-//use long to hold value
-int reverse(int x) {
     long rev = 0;
     while (x != 0) {
         int back = x % 10;
@@ -33,7 +16,7 @@ int reverse(int x) {
     return rev;
 }
 
-//This is what I would give
+// uses reverse command
 int reverse(int x) {
     int reverse(int x) {
         string s = to_string(x);
