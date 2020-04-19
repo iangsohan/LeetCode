d3628a9ea3812ea04e4929e41fe3ea0430fdfddc
iangsohan <iangsohan@gmail.com>
2020-04-19 17:33:22 -0400

    Update TwoSum.cpp
    Added unordered map option and simplified original.

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,20 +1,33 @@
 /*	LeetCode: "Two Sum"
-	- Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-	- You may assume that each input would have exactly one solution, and you may not use the same element twice.	*/
+    - Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+    - You may assume that each input would have exactly one solution, and you may not use the same element twice.	*/
 
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
+
+// unordered_map option
 vector<int> twoSum(vector<int>& nums, int target) {
-    vector<int> ans;
+    unordered_map<int, int> umap;
+    for (int i = 0; i < nums.size(); i++) {
+        if (umap.find(target - nums[i]) != umap.end()) {
+            return { umap[nums[i]], i };
+        }
+        umap[nums[i]] = i;
+    }
+    return {};
+}
+
+// for loop option
+vector<int> twoSum(vector<int>& nums, int target) {
     for (int i = 0; i < nums.size() - 1; i++) {
         for (int j = i + 1; j < nums.size(); j++) {
             if (nums[i] + nums[j] == target) {
-                ans.push_back(i);
-                ans.push_back(j);
+                return { i, j };
             }
         }
     }
-    return ans;
+    return {};
 }
