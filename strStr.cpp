16ac928b96fb26cf41ca84304f7b8ad6797b888a
iangsohan <iangsohan@gmail.com>
2020-04-22 20:49:46 -0400

    Update strStr.cpp
    Previous code was impressively slow at O(n!).

--- strStr.cpp
+++ strStr.cpp
@@ -5,27 +5,13 @@
 #include<string>
 using namespace std;
 
-
-// Correct, but too slow!
-int strStr(string h, string n) {
-    if (n == "")
+int strStr(string haystack, string needle) {
+    int nLen = needle.length();
+    if (nLen == 0)
         return 0;
-    for (int i = 0; i < h.length(); i++) {
-        if (h[i] == n[0]) {
-            int x = strStr(h.substr(i + 1, h.length() - 1), n.substr(1, n.length() - 1));
-            if (x == 0) {
-                return i;
-            } else if (x == -1) {
-                return -1;
-            }
-        }
+    for (int i = 0; i < haystack.length(); i++) {
+        if (haystack.substr(i, nLen).compare(needle) == 0)
+            return i;
     }
     return -1;
-}
-
-int main() {
-    cout << strStr("mississippi", 
-        "issip") << endl;
-
-    return 0;
 }
