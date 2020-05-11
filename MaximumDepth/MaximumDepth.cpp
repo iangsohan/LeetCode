e0ce9abb3dc84288a8a719b1742f25b46a3209ff
Ian Sohan <iangsohan@gmail.com>
2020-05-11 11:09:13 -0400

    Create MaximumDepth.cpp
    Given a binary tree, finds its maximum depth.

---
+++ MaximumDepth/MaximumDepth.cpp
@@ -0,0 +1,11 @@
+/*  LeetCode - "Maximum Depth of Binary Tree"
+    - Given a binary tree, find its maximum depth.  */
+
+#include<iostream>
+#include "TreeNode.h"
+using namespace std;
+
+int maxDepth(TreeNode* root) {
+    if (root == nullptr) return 0;
+    return 1 + max(maxDepth(root->getLeft()), maxDepth(root->getRight()));
+}
