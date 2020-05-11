a41ba4ebdf405324b1beb105ecac1b34b6485fba
Ian Sohan <iangsohan@gmail.com>
2020-05-11 11:07:36 -0400

    Create BinaryTreeLevelOrderTraversal2.cpp
    Given a binary tree, returns the bottom-up level order traversal of its nodes' values.

---
+++ BinaryTreeLevelOrderTraversal2/BinaryTreeLevelOrderTraversal2.cpp
@@ -0,0 +1,21 @@
+/*  LeetCode - "Binary Tree Level Order Traversal II"
+    - Given a binary tree, return the bottom-up level order traversal of its nodes' values. */
+
+#include<iostream>
+#include<vector>
+#include "TreeNode.h"
+
+void levelOrderHelper(TreeNode* root, vector<vector<int>>& level, int height) {
+    if (root == nullptr) return;
+    if (height >= level.size()) level.push_back({});
+    level[height].push_back(root->getVal());
+    levelOrderHelper(root->getLeft(), level, height+1);
+    levelOrderHelper(root->getRight(), level, height+1);
+}
+
+vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    vector<vector<int>> level {};
+    levelOrderHelper(root, level, 0);
+    reverse(level.begin(), level.end());
+    return level;
+}
