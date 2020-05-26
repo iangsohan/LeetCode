59909f04d9b86afe7553e86dbad15a379bc3c401
Ian Sohan <iangsohan@gmail.com>
2020-05-26 14:46:18 -0400

    Update 3Sum.cpp
    Reduced runtime.

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -7,28 +7,28 @@
 #include<algorithm>
 using namespace std;
 
-void twoSum(vector<vector<int>>& triplets, vector<int> nums, int target, int start) {
-    unordered_set<int> sum;
-    unordered_set<int> end;
-    for (int i = start+1; i < nums.size(); i++) {
-        if (sum.count(target-nums[i]) && !end.count(nums[i])) {
-            triplets.push_back({nums[start], target-nums[i], nums[i]});
-            end.insert(nums[i]);
+vector<vector<int> > threeSum(vector<int> &num) {
+    vector<vector<int>> triplets;
+    sort(num.begin(), num.end());
+    for (int i = 0; i < num.size(); i++) {
+        int target = -num[i];
+        int front = i + 1, back = num.size() - 1;
+        while (front < back) {
+            int sum = num[front] + num[back];
+            if (sum < target)
+                front++;
+            else if (sum > target)
+                back--;
+            else {
+                triplets.push_back({num[i], num[front], num[back]});
+                while (front < back && num[front] == triplets.back()[1])
+                    front++;
+                while (front < back && num[back] == triplets.back()[2])
+                    back--;
+            }
         }
-        sum.insert(nums[i]);
-    }
-}
-
-vector<vector<int>> threeSum(vector<int>& nums) {
-    vector<vector<int>> triplets {};
-    if (nums.size() < 3) return triplets;
-    sort(nums.begin(), nums.end());
-    twoSum(triplets, nums, -1 * nums[0], 0);
-    for (int i = 1; i < nums.size()-2; i++) {
-        while (nums[i] == nums[i-1] && i < nums.size()-2) i++;
-        if (i == nums.size()-2) break;
-        int target = nums[i];
-        twoSum(triplets, nums, -1 * target, i);
+        while (i+1 < num.size() && num[i+1] == num[i]) 
+            i++;
     }
     return triplets;
 }
